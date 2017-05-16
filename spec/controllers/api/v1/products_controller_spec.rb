7f5914391393f1da28bef6a9271878d681479eb7
saichander <saichander.ramaram@gmail.com>
2017-05-16 20:31:12 +0530

    added show, index product end points

---
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Api::V1::ProductsController, type: :controller do
+  describe "GET #show" do
+    before(:each) do
+      @product = FactoryGirl.create :product
+      get :show, id: @product.id
+    end
+
+    it "returns the information about a reporter on a hash" do
+      product_response = json_response
+      expect(product_response[:title]).to eql @product.title
+    end
+
+  end
+
+end
